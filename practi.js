b7eda393984b2d821ae067ceee3e99a561f5732d
olwpad <olwpad@gmail.com>
2023-05-14 10:01:37 -0500

    Updates

---
+++ practi.js
@@ -0,0 +1,10 @@
+"use strict";
+let pan = 5;
+let pan1 = 8;
+console.log(pan + pan1);
+let panseroti = (a, b) => a + b;
+console.log(panseroti(pan1, pan));
+function yo(a, b) {
+    return a + b;
+}
+console.log(yo(pan, pan1));
